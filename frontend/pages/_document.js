188735f2456f8db6c65201efd268ffd1084a6887
Greg Thompson <thompson.glowe@gmail.com>
2018-12-01 11:31:53 -0600

    pinterest site verification

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -18,10 +18,7 @@ export default class MyDocument extends Document {
             <title>
                 Joanna Thompson | Professional Organizing | Omaha, NE
             </title>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-            />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
             <meta name="Description" content="In-home and remote professional organizing. I will help you understand your own organizing weaknesses and challenges, and together we will create the stress-free, organized environment you want." />
             <meta property="og:site_name" content="Joanna Thompson" />
@@ -30,6 +27,7 @@ export default class MyDocument extends Document {
             <meta property="og:type" content="website" />
             <meta property="og:image" content="https://wp.joanna-thompson.com/wp-content/uploads/2018/10/boxes.jpg" />
             <meta name="google-site-verification" content="_pGpTGZEnuUGx1YU27atkiyzldRegtPFLsIdbBF7Eb0" />
+            <meta name="p:domain_verify" content="1208501e82b80ef57111d9ad0964fa4a" />
             <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
             {this.props.styleTags}
         </Head>
